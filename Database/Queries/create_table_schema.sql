3bd63fc8840d96396035cadf72af1a64fbbd5d8b
Michael Mishkanian <77469811+Mishkanian@users.noreply.github.com>
2021-06-16 16:59:39 -0700

    Updated schema table queries

--- Database/Queries/create_table_schema.sql
+++ Database/Queries/create_table_schema.sql
@@ -1,6 +1,6 @@
 -- The following queries will create the necessary tables for this project.
 -- These queries must be run in this exact order.
--- Data must be imported in the exact sequence below as well.
+-- Data must be imported in the exact table sequence below as well.
 
 CREATE TABLE "dspi_data" (
     "Date" date   NOT NULL,
@@ -14,37 +14,35 @@ CREATE TABLE "dspi_ratio" (
     FOREIGN KEY("Date") REFERENCES "dspi_data" ("Date")
 );
 
-CREATE TABLE "features" (
-    "Store" int   NOT NULL,
-    "Date" date   NOT NULL,
-    "Temperature" int   NOT NULL,
-    "Fuel_Price" int   NOT NULL,
-    "MarkDown1" int   NOT NULL,
-    "MarkDown2" int   NOT NULL,
-    "MarkDown3" int   NOT NULL,
-    "MarkDown4" int   NOT NULL,
-    "MarkDown5" int   NOT NULL,
-    "CPI" int   NOT NULL,
-    "Unemployment" int   NOT NULL,
-    "IsHoliday" bool   NOT NULL,
-    PRIMARY KEY ("Store")
-);
-
 CREATE TABLE "stores_data" (
     "Store" int   NOT NULL,
     "Type" varchar   NOT NULL,
     "Size" int   NOT NULL,
-    PRIMARY KEY ("Store"),
-    FOREIGN KEY("Store") REFERENCES "features" ("Store")
+    PRIMARY KEY ("Store")
+);
+
+CREATE TABLE "features" (
+    "Store" int   NOT NULL,
+    "Date" date   NOT NULL,
+    "Temperature" numeric   NOT NULL,
+    "Fuel_Price" numeric   NOT NULL,
+    "MarkDown1" numeric   NOT NULL,
+    "MarkDown2" numeric   NOT NULL,
+    "MarkDown3" numeric   NOT NULL,
+    "MarkDown4" numeric   NOT NULL,
+    "MarkDown5" numeric   NOT NULL,
+    "CPI" numeric   NOT NULL,
+    "Unemployment" numeric   NOT NULL,
+    "IsHoliday" bool   NOT NULL,
+    FOREIGN KEY("Store") REFERENCES "stores_data" ("Store")
 );
 
 CREATE TABLE "sales_data" (
     "Store" int   NOT NULL,
     "Dept" int   NOT NULL,
     "Date" date   NOT NULL,
-    "Weekly_Sales" int   NOT NULL,
+    "Weekly_Sales" numeric   NOT NULL,
     "IsHoliday" bool   NOT NULL,
-    PRIMARY KEY ("Store"),
     FOREIGN KEY("Store") REFERENCES "stores_data" ("Store")
 );
 
@@ -52,9 +50,14 @@ CREATE TABLE "sales_data" (
 -- DROP SCHEMA public CASCADE;
 -- CREATE SCHEMA public;
 
--- Check to make sure all tables are generated correctly
--- SELECT * FROM dspi_data;
--- SELECT * FROM dspi_ratio;
--- SELECT * FROM features;
--- SELECT * FROM sales_data;
--- SELECT * FROM stores_data;
+-- To drop a single table, use the query below
+-- DROP TABLE sales_data;
+-- DROP TABLE stores_data;
+-- DROP TABLE features;
+
+-- Check to make sure all of the tables generated correctly
+SELECT * FROM dspi_data;
+SELECT * FROM dspi_ratio;
+SELECT * FROM features;
+SELECT * FROM sales_data;
+SELECT * FROM stores_data;
